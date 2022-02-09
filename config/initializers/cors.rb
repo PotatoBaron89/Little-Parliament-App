abfdb6f4a8f9eda2a75ac6cb689b6d78d68b0c82
samboy29@gmail.com <samboy29@gmail.com>
2022-02-09 21:44:40 +1000

    Fixed bug, origin v origins typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,13 +2,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origin 'http://localhost:3000'
+    origins 'http://localhost:3000'
     resource '*', headers: :any, methods: %i[get post put patch delete options head],
                   credentials: true
   end
 
   allow do
-    origin 'https://gentle-fortress-16563.herokuapp.com/'
+    origins 'https://gentle-fortress-16563.herokuapp.com/'
     resource '*', headers: :any, methods: %i[get post put patch delete options head],
                   credentials: true
   end
