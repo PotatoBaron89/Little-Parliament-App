23e2a20ebcb423db5fb38145c1a4c35a6d5f7ec2
samboy29@gmail.com <samboy29@gmail.com>
2022-02-10 20:09:45 +1000

    Trying to fix session store on master

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://gentle-fortress-16563.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'gentle-fortress-16563.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
