2ba7c60518226f75277f15d72e69f5eccc89f8f0
samboy29@gmail.com <samboy29@gmail.com>
2022-02-12 18:44:21 +1000

    Removed fatal bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   include CurrentUserConcern
-  session[:user_id] = users(:one).id
 
   def authenticate_user
     if @current_user
