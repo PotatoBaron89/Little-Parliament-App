ebb2db3e380ad18dbfcc3413847045a5343acf7d
samboy29@gmail.com <samboy29@gmail.com>
2022-02-13 18:46:20 +1000

    Small changes to tests

--- test/controllers/drinks_controller_test.rb
+++ test/controllers/drinks_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class DrinksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @drink = drinks(:one)
+    @current_user = users(:one)
+    @current_user.add_role :admin
   end
 
   test 'should get index' do
@@ -10,14 +12,8 @@ class DrinksControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_drink_url
-    assert_response :success
-  end
 
   test 'should create drink' do
-    @current_user = users(:one)
-    @current_user.add_role :admin
 
     assert_difference('Drink.count') do
       post drinks_url, params: { drink: { description: @drink.description, price: @drink.price, title: @drink.title } }
@@ -37,9 +33,6 @@ class DrinksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update drink' do
-    @current_user = users(:one)
-    @current_user.add_role :admin
-
     patch drink_url(@drink), params: { drink: { description: @drink.description, price: @drink.price, title: @drink.title } }
     assert_response :success
   end
